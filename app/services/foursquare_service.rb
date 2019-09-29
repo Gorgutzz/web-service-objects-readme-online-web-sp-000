ea7d899541d8431f34f3b840e3c4357609a0517f
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-09-29 04:00:30 +0000

    Automatically backed up by Learn

--- app/services/foursquare_service.rb
+++ app/services/foursquare_service.rb
@@ -1,23 +1,22 @@
 class FoursquareService
-    def authenticate!(client_id, client_secret, code)
-        resp = Faraday.get("https://foursquare.com/oauth2/access_token") do |req|
-          req.params['client_id'] = client_id
-          req.params['client_secret'] = client_secret
-          req.params['grant_type'] = 'authorization_code'
-          req.params['redirect_uri'] = "http://localhost:3000/auth"
-          req.params['code'] = code
-        end
-        body = JSON.parse(resp.body)
-        body["access_token"]
-    end
+  def authenticate!(client_id, client_secret, code)
+    def create
+      resp = Faraday.post("https://github.com/login/oauth/access_token") do |req|
+        req.body = { 'client_id': ENV['GITHUB_CLIENT'], 'client_secret': ENV['GITHUB_SECRET'], 'code': params[:code]}
+        req.headers['Accept'] = 'application/json'
+      end
 
-    def friends(token)
-        resp = Faraday.get("https://api.foursquare.com/v2/users/self/friends") do |req|
-          req.params['oauth_token'] = token
-          # don't forget that pesky v param for versioning
-          req.params['v'] = '20190914'
-        end
-        @friends = JSON.parse(resp.body)["response"]["friends"]["items"]
+      body = JSON.parse(resp.body)
+      body['access_token']
     end
+  end
 
+  def friends(token)
+    resp = Faraday.get("https://api.foursquare.com/v2/users/self/friends") do |req|
+      req.params['oauth_token'] = token
+      # don't forget that pesky v param for versioning
+      req.params['v'] = '20160201'
+    end
+    JSON.parse(resp.body)["response"]["friends"]["items"]
+  end
 end
